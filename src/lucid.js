5f2214bd9133953ac668cb365783cb9bdcf9ba16
Nick Wing <nwing@txstate.edu>
2021-09-29 09:18:08 -0500

    when an image is in multiple albums, choose the shortest path instead of giving it to lucid twice

--- src/lucid.js
+++ src/lucid.js
@@ -1,4 +1,4 @@
-import { isNotNull } from 'txstate-utils'
+import { isNotNull, sortby } from 'txstate-utils'
 const LUCID_MAXRES = parseInt(process.env.LUCID_MAXRES ?? '4000')
 const LUCID_MAXMB = parseInt(process.env.LUCID_MAXMB ?? '8')
 
@@ -6,19 +6,19 @@ export class LucidData {
   constructor () {
     this.version = 1
     this.folders = {}
+    this.imagesSeen = new Set()
   }
 
   addImage (img) {
-    if (!img.id || !img.name) return
-    for (const path of img.folders) {
-      let lastfolder = this
-      for (const folder of path) {
-        lastfolder.folders[folder.id] ??= new LucidFolder(folder.id, folder.name)
-        lastfolder = lastfolder.folders[folder.id]
-      }
-      lastfolder.images.push(img)
+    if (!img.id || !img.name || !img.folderPath?.length || this.imagesSeen.has(img.id)) return
+    let lastfolder = this
+    for (const folder of img.folderPath) {
+      lastfolder.folders[folder.id] ??= new LucidFolder(folder.id, folder.name)
+      lastfolder = lastfolder.folders[folder.id]
     }
-    delete img.folders
+    this.imagesSeen.add(img.id)
+    lastfolder.images.push(img)
+    delete img.folderPath
   }
 
   json () {
@@ -71,9 +71,27 @@ export class LucidImage {
     }
 
     this.thumbnailUrl = img.url.preview
+
+    const folderPaths = sortby(
+      img.relatedAlbums?.map(album => {
+        const ids = album.idPath.split('/')
+        const names = album.namePath.split('/')
+        if (ids.length !== names.length) console.error('Found an album with a / in its name.')
+        return ids.map((id, i) => ({ id, name: names[i] }))
+      }) ?? [],
+      p => p.length,
+      p => p[p.length - 1].id
+    )
+
+    this.folderPath = folderPaths[0]
+
+    const otherfolders = folderPaths.filter(p => p[p.length - 1].id !== this.folderPath[this.folderPath.length - 1].id)
+      .map(p => p[p.length - 1].name)
+
     const seen = new Set()
     this.tags = (img.tag ?? [])
       .concat(img.keyword ?? [])
+      .concat(otherfolders)
       .concat(Object.values(img.additional).filter(isNotNull))
       .map(t => t.trim())
       .filter(t => t !== 'Untagged')
@@ -83,11 +101,5 @@ export class LucidImage {
         seen.add(lc)
         return !dupe
       })
-    this.folders = img.relatedAlbums?.map(album => {
-      const ids = album.idPath.split('/')
-      const names = album.namePath.split('/')
-      if (ids.length !== names.length) console.error('Found an album with a / in its name.')
-      return ids.map((id, i) => ({ id, name: names[i] }))
-    }) ?? []
   }
 }
